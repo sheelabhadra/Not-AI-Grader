98eb7846db08affb659163188de7d44a249b6ca1
Sheelabhadra Dey <sheelabhadra@gmail.com>
2020-11-03 17:38:37 -0600

    Updated bash script

--- grade.sh
+++ grade.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# python create_project_dir.py -src ../submissions/proj2/ -dst proj2/ -p P2
-# rm -rf __pycache__
+python create_project_dir.py -src ../submissions/proj2/ -dst proj2/ -p P2
+rm -rf __pycache__
 
-# for project in */ ; do
-# 	for student in $project*; do
-# 		echo $student >> grades-P2.txt
-# 		cd $student
-# 		# python autograder.py >> ../../grades-P1.txt
-# 		python main.py >> ../../grades-P2.txt
-# 		cd "$OLDPWD"
-# 	done
-# done
+for project in */ ; do
+	for student in $project*; do
+		echo $student >> grades.txt
+		cd $student
+		python autograder.py >> ../../grades.txt
+		cd "$OLDPWD"
+	done
+done
 
-# python save_grades.py
+python save_grades.py -dst proj2/
